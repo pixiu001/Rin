89e51d6fd84ca053cbd58a08761c9a58344cca65
pixiu001 <153519193+pixiu001@users.noreply.github.com>
2026-01-18 20:20:14 +0800

    Update footer.tsx

--- client/src/components/footer.tsx
+++ client/src/components/footer.tsx
@@ -59,7 +59,7 @@ function Footer() {
                             setDoubleClickTimes(doubleClickTimes + 1)
                         }
                     }}>
-                        © {new Date().getFullYear()} Powered by <a className='hover:underline' href="https://github.com/openRin/Rin" target="_blank">Rin</a>
+                        © {new Date().getFullYear()} Powered by <a className='hover:underline' href="/" target="_blank">xinchen</a>
                     </span>
                     {config.get<boolean>('rss') && <>
                         <Spliter />
